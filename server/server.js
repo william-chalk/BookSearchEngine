bcbbf631c2e182206e860931194f9307419327cd
William Chalk <william@printedproducts.com>
2023-01-03 13:53:02 -0500

    implemented graphql in server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +22,19 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../client/build/index.html"));
 });
+
+const startApolloServer = async(typeDefs,resolvers)=>{
+  await server.start();
+  server.applyMiddleware({app});
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
+
+startApolloServer(typeDefs,resolvers);
+
